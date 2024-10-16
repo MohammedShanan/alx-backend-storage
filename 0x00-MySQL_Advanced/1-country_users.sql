14a40be744d7b0ae61a0f51758ff7e05330a2a00
mohammedshanan <mohammedshana12@gmail.com>
2024-10-16 11:13:32 +0300

    added 2-fans.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,4 @@
+-- email addresses and a country code restricted to 'US', 'CO', or 'TN'.
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
